839a3f1614b942934deba4416d59a89734a4e68d
Arpit Thool <arpitthool@vt.edu>
2025-10-22 22:22:23 -0400

    - initial canvas setup 	modified:   src/Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -1,8 +1,10 @@
-import './index.css';
-
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 if (!ctx) {
   throw new Error('Could not get context');
 }
+
+const score = document.getElementById('score') as HTMLSpanElement;
+
+score.textContent = 'Score : 1';
